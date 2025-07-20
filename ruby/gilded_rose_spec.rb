d839d69a4ab3c0eeb9a52d0cb90ec16775f6f4ef
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2025-07-20 10:21:53 -0400

    Fix rubocop warnings

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rspec'
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "foo"
+  it 'does not change the name' do
+    items = [Item.new('foo', 0, 0)]
+    GildedRose.new(items).update_quality
+    expect(items[0].name).to eq 'fixme'
   end
 end
